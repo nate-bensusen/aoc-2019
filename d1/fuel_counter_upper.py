66961897cd97282bd8d30cea85c52822e0717916
Nate Bensusen <nate@wewwl.com>
2019-12-02 20:47:17 -0800

    .

---
+++ d1/fuel_counter_upper.py
@@ -0,0 +1,18 @@
+# What am I doing?
+# Firstly I need to get the list of module weights. How should I do that? How should I import that list? How am I going to 
+
+import json
+
+get list
+
+module_list_imported = [json.loads()
+]
+
+
+for x in list
+run this equation
+	in equation I need to do this
+		take x /3 drop remainder subtract 2
+	add to final list
+
+print final list
